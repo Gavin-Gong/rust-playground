c8f9c5d8f698446f04e0b72a7d4f1f11ca354b1e
Gavin Gong <kefengong@gmail.com>
2018-11-17 17:06:49 +0800

    update

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,5 +1,8 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8890").unwrap();
@@ -12,12 +15,28 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let reponse = format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{}",
-        String::from("{ \"hello\": \"Rust\" }")
-    );
-    println!("{}", reponse);
-    stream.write(reponse.as_bytes()).unwrap();
+    let (status_line, file_path) = if buffer.starts_with(get) {
+        (
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n",
+            "data/resp.json",
+        )
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        (
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n",
+            "data/resp.json",
+        )
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "data/404.html")
+    };
+    let mut file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let resp = format!("{}{}", status_line, contents);
+    stream.write(resp.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
